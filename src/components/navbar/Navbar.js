d25a57d8e5867ed3ca7ec873227341557ffe9167
BAnupam <75614326+BAnupam@users.noreply.github.com>
2021-06-01 14:44:38 +0530

    Add files via upload

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { AppBar, Toolbar, CssBaseline } from '@material-ui/core';
+import { AppBar, Toolbar, CssBaseline, Typography, List, ListItem, Drawer } from '@material-ui/core';
+import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
     <div>
       <CssBaseline />
-      <AppBar style={{ width: 'calc(100% - 50px)', backgroundColor: 'red' }}>
-        <Toolbar style={{ marginLeft: 'auto', backgroundColor: 'red' }}>
-          <img className="icon-color" src="/assets/logout-1.svg" alt="" />
+      <AppBar className={styles.navbar} >
+      <CssBaseline />
+        <Toolbar className={styles.navTool}>
+          <img className={styles.navIcon} src="/assets/search-big.svg" alt="" />
+          <img className={styles.navIcon} src="/assets/notification.svg" alt="" />
+          <img className={styles.navIcon} src="/assets/logout-2.svg" alt="" />
         </Toolbar>
       </AppBar>
     </div>
